69812fff0e3a16c09a8acfd4669d4f6b8266d89d
Uzgur <uzbmv2@gmail.com>
2023-11-30 02:25:35 +0500

    auth bitti

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,51 +1,73 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes as Switch, Route, Navigate, Link } from 'react-router-dom';
+import React, { ChangeEventHandler, useState } from "react";
+import axios from "axios";
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route,
+  Navigate,
+  Link,
+  useNavigate,
+} from "react-router-dom";
 export function Register() {
-	function showInput(event: any) {
-		let inputValue = event.target.value;
-		console.log(inputValue);
-	}
+  const navigate = useNavigate();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-	return (
-		<form className="container w-50 login">
-			<h1>Register</h1>
-			{/* <!-- User Name input --> */}
-			<div className="form-outline mb-4">
-				<span>Email:</span>
-				<input
-					type="email"
-					id="form2Example1"
-					className="form-control logEmail"
-					onInput={showInput}
-				/>
-			</div>
+  async function handleSubmit(event: any) {
+    event.preventDefault();
+    const { data } = await axios.post("http://localhost:4000/api/users", {
+      name,
+      email,
+      password,
+    });
 
-			{/* <!-- UserName input --> */}
-			<span>Username:</span>
-			<div className="form-outline mb-4">
-				<input
-					type="text"
-					id="form2Example2"
-					className="form-control logName"
-					onInput={showInput}
-				/>
-			</div>
+    console.log("data: ", data);
+    console.log(name, email, password);
 
-			{/* <!-- Password input --> */}
-			<div className="form-outline mb-4">
-				<span>Password:</span>
-				<input
-					type="password"
-					id="form2Example2"
-					className="form-control logPassword"
-					onInput={showInput}
-				/>
-			</div>
+    navigate("/login");
+  }
 
-			{/* <!-- Submit button --> */}
-			<button type="button" className="btn btn-primary btn-block mb-4 sing-in">
-				Sign in
-			</button>
-		</form>
-	);
+  return (
+    <form className="container w-50 login" onSubmit={handleSubmit}>
+      <h1>Register</h1>
+      {/* <!-- User Name input --> */}
+      <div className="form-outline mb-4">
+        <span>Email:</span>
+        <input
+          type="email"
+          id="form2Example1"
+          className="form-control logEmail"
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </div>
+
+      {/* <!-- UserName input --> */}
+      <span>Username:</span>
+      <div className="form-outline mb-4">
+        <input
+          type="text"
+          id="form2Example2"
+          className="form-control logName"
+          onChange={(event) => setName(event.target.value)}
+        />
+      </div>
+
+      {/* <!-- Password input --> */}
+      <div className="form-outline mb-4">
+        <span>Password:</span>
+        <input
+          type="password"
+          id="form2Example3"
+          className="form-control logPassword"
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </div>
+
+      {/* <!-- Submit button --> */}
+      <button type="submit" className="btn btn-primary btn-block mb-4 sing-in">
+        Sign in
+      </button>
+    </form>
+  );
 }
