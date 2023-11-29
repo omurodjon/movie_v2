b5bf88fbab524201c3078d90d69da34ca7de47bd
omurodjon <famiylinfomy@gmail.com>
2023-11-30 00:20:37 +0500

    Register page updated

--- src/components/register.tsx
+++ src/components/register.tsx
@@ -1,25 +1,45 @@
 import React from 'react';
-
+import { BrowserRouter as Router, Routes as Switch, Route, Navigate, Link } from 'react-router-dom';
 export function Register() {
+	function showInput(event: any) {
+		let inputValue = event.target.value;
+		console.log(inputValue);
+	}
+
 	return (
 		<form className="container w-50 login">
 			<h1>Register</h1>
 			{/* <!-- User Name input --> */}
 			<div className="form-outline mb-4">
 				<span>Email:</span>
-				<input type="email" id="form2Example1" className="form-control logEmail" />
+				<input
+					type="email"
+					id="form2Example1"
+					className="form-control logEmail"
+					onInput={showInput}
+				/>
 			</div>
 
 			{/* <!-- UserName input --> */}
 			<span>Username:</span>
 			<div className="form-outline mb-4">
-				<input type="text" id="form2Example2" className="form-control logName" />
+				<input
+					type="text"
+					id="form2Example2"
+					className="form-control logName"
+					onInput={showInput}
+				/>
 			</div>
 
 			{/* <!-- Password input --> */}
 			<div className="form-outline mb-4">
 				<span>Password:</span>
-				<input type="password" id="form2Example2" className="form-control logPassword" />
+				<input
+					type="password"
+					id="form2Example2"
+					className="form-control logPassword"
+					onInput={showInput}
+				/>
 			</div>
 
 			{/* <!-- Submit button --> */}
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,21 +1,15 @@
-import {
-    BrowserRouter as Router,
-    Routes as Switch,
-    Route,
-    Navigate,
-  } from "react-router-dom";
-import { Login } from "../components/login";
-import { Register } from "../components/register";
-import App  from '../App'
-  const Routes = () => (
-    <Router>
-      <Switch>
-        <Route index element={<App />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Switch>
-    </Router>
-  );
-  
+import { BrowserRouter as Router, Routes as Switch, Route } from 'react-router-dom';
+import { Login } from '../components/login';
+import { Register } from '../components/register';
+import App from '../App';
+const Routes = () => (
+	<Router>
+		<Switch>
+			<Route index element={<App />} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/register" element={<Register />} />
+		</Switch>
+	</Router>
+);
+
 export default Routes;
-  
